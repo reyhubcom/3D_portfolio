31d24d331856a21445bec3f531053b5410b3c94e
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2023-11-12 14:59:36 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -112,7 +112,7 @@ export const skills = [
 export const experiences = [
     {
         title: "React.js Developer",
-        company_name: "Starbucks",
+        company_name: "Web3 Wallet",
         icon: starbucks,
         iconBg: "#accbe1",
         date: "March 2020 - April 2021",
