f9e1fec3c5e540671a5a693ce7ef30426c4b26d6
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2023-11-17 21:52:57 -0500

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -22,12 +22,12 @@ const About = () => {
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
         <p>
-        Blockchain technology is an advanced database mechanism that allows transparent information sharing within a business network. A blockchain database stores .
+        Peerless, actionable, multichain data and coverage on the widest selection of dapps, NFT collections, DeFi projects and more. View top collections, trending dapps, trader volume and maybe, just maybe, uncover the next blockchain unicorn.
         </p>
       </div>
 
       <div className='py-10 flex flex-col'>
-        <h3 className='subhead-text'>List Of Blockchains</h3>
+        <h3 className='subhead-text'>For More To Explore</h3>
 
         <div className='mt-16 flex flex-wrap gap-12'>
           {skills.map((skill) => (
@@ -54,7 +54,60 @@ const About = () => {
           </p>
         </div>
 
-        
+        <div className='mt-12 flex'>
+          <VerticalTimeline>
+            {experiences.map((experience, index) => (
+              <VerticalTimelineElement
+                key={experience.company_name}
+                date={experience.date}
+                iconStyle={{ background: experience.iconBg }}
+                icon={
+                  <div className='flex justify-center items-center w-full h-full'>
+                    <img
+                      src={experience.icon}
+                      alt={experience.company_name}
+                      className='w-[60%] h-[60%] object-contain'
+                    />
+                  </div>
+                }
+                contentStyle={{
+                  borderBottom: "8px",
+                  borderStyle: "solid",
+                  borderBottomColor: experience.iconBg,
+                  boxShadow: "none",
+                }}
+              >
+                <div>
+                  <h3 className='text-black text-xl font-poppins font-semibold'>
+                    {experience.title}
+                  </h3>
+                  <p
+                    className='text-black-500 font-medium text-base'
+                    style={{ margin: 0 }}
+                  >
+                    {experience.company_name}
+                  </p>
+                </div>
+
+                <ul className='my-5 list-disc ml-5 space-y-2'>
+                  {experience.points.map((point, index) => (
+                    <li
+                      key={`experience-point-${index}`}
+                      className='text-black-500/50 font-normal pl-1 text-sm'
+                    >
+                      {point}
+                    </li>
+                  ))}
+                </ul>
+              </VerticalTimelineElement>
+            ))}
+          </VerticalTimeline>
+        </div>
+      </div>
+
+      <hr className='border-slate-200' />
+
+      <CTA />
     </section>
   );
 };
